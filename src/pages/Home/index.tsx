ef56df0e12df601edc90dc3176b9bc6f8bfcd98e
Barbara <babisolua@gmail.com>
2024-10-30 16:14:05 -0300

    :sparkles:	feat: add props para butão

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,17 @@
 import { ButtonComecar } from '../../components/ButtonComecar'
 import { Header } from '../../components/Header'
 import styles from './home.module.css'
+
 export const Home: React.FC = () =>{
 
     return(
         <div className={styles.div}>
-        <Header/>
+        <Header/> 
 
         <div className={styles.descricao}>
         <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae eleifend eu eleifend ac vel elit malesuada fames. Semper luctus volutpat tempor arcu.</p>
-        <ButtonComecar/>
+        <ButtonComecar texto='Começar'/>
         </div>
         <img src="./Home-Indicator.png" alt="incador de página" className={styles.indicador}/>
         </div>
