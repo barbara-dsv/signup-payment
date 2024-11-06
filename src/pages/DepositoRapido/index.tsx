51e806b0cf38120257f19da7643bf58aa1fcf937
Barbara <babisolua@gmail.com>
2024-11-06 16:28:39 -0300

    :sparkles:	feat: add estrutura de pagina de deposito rapido

--- src/pages/DepositoRapido/index.tsx
+++ src/pages/DepositoRapido/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './depositoRapido.module.css'
 import { Header } from '../../components/Header'
 import { FrameTopo } from '../../components/FrameTopo'
+import { Button } from '../../components/Button'
+
 export const DepositoRapido: React.FC = () =>{
     
     return(
@@ -10,8 +12,25 @@ export const DepositoRapido: React.FC = () =>{
         <main className={styles.depositoRapido}>
         <FrameTopo texto='Deposito Rapido'/>
         <h1 className={styles.descricaoPagina}>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
-        </main>
 
+        <div className={styles.valorInicial}>
+          <h2>R$ 20,00</h2>
+          <p>Max. R$ 999,99/mês</p>
+          <div className={styles.opcoesDeValores}>
+          <button>+ R$ 50,00</button>
+          <button>+ R$ 100,00</button>
+          <button>+ R$ 700,00</button>
+        </div>
+        </div>
+
+        <footer className={styles.rodape}>
+        <p className={styles.termo}><a href="">Morbi vitae eleifend</a> eu eleifend ac vel elit. </p>
+        <Button texto='Pagar com pix'/>
+        </footer>
+       
+
+        </main>
+        <img src="./Home-Indicator.png" alt="incador de página" className={styles.indicador}/>
       </div>
     )
 }
