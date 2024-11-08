91524ebd718b0ce48250e451bbcabe036cd03abf
barbara <babiiufc@gmail.com>
2024-11-08 16:21:23 -0300

    :truck: chore:  função de onClick movida para props para poder ser utilizada em qualquer página

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,21 +1,17 @@
 import React from "react";
 import { PiArrowRightBold } from "react-icons/pi";
 import styles from './button.module.css'
-import { useNavigate } from "react-router-dom";
 
 interface ButtonProps{
     texto: string;
+    onClick?: () => void;
 }
 
-export const Button: React.FC<ButtonProps> = ({texto}) =>{
-    const navigate = useNavigate()
-    function navegarParaOutraPagina() {
-    
-        navigate('/cadastro')
-    }
+export const Button: React.FC<ButtonProps> = ({texto, onClick}) =>{
+   
     
     return(
-        <button className={styles.btn_comecar} onClick={navegarParaOutraPagina}>
+        <button className={styles.btn_comecar} onClick={onClick}>
             <p className={styles.comecar}>{texto}</p>
             <PiArrowRightBold size={18} className={styles.setaDireita}/>
         </button>
