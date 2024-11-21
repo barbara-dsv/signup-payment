0fa1cd67577fe2becab63c1023e1fa852791478c
barbara <babiiufc@gmail.com>
2024-11-21 10:50:24 -0300

    :sparkles:	feat: página de erro no pix finaliada

--- src/pages/ErroPix/index.tsx
+++ src/pages/ErroPix/index.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import styles from './erroPix.module.css'
 import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { useNavigate } from "react-router-dom";
 
 export const ErroPix: React.FC = () =>{
+    const navigate = useNavigate()
+
+    const handleErro = ()=>{
+        navigate('/depositoPix')
+    }
+
     return(
         <div className={styles.container}>
         <Header/>
         <main className={styles.erroPix}>
+            <div>
             <h1 className={styles.msgErro}>Falha no pagamento</h1>
             <p className={styles.descricaoErro}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
             Morbi vitae eleifend eu eleifend ac vel elit malesuada fames. Semper luctus volutpat tempor arcu.
             </p>
+            </div>
+            <Button texto="Tentar novamente" onClick={handleErro}/>
         </main>
         <img src="./Home-Indicator.png" alt="incador de página" className={styles.indicador}/>
     </div>
